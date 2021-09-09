5d2c3c0b4bf34875fe011a2352ee488dbb627810
dhananjaychoudhari <76993420+dhananjaychoudhari@users.noreply.github.com>
2021-09-09 18:55:03 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,31 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import '../styles/App.css';
 
 const App = () => {
-  // write your code here 
+  const [time1, setTime1] = useState();
 
+  useEffect(() => {
+    let x = setInterval(() => {
+      setTime1(time1 - 1);
+    }, 1000)
+    return () => {
+      clearInterval(x);
+    }
+  }, [time1])
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={(event) => {
+            let no = Number(event.target.value);
+            setTime1(Math.floor(no));
+          }} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{
+        (isNaN(time1) || time1 <= 0) ? 0 : Math.floor(time1)
+        // time1
+      }</div>
     </div>
   )
 }
